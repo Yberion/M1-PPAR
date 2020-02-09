2d8f7a6fd8fe0a1410bcde652bd4e839db078be3
Brandon <yberion.ins@gmail.com>
2020-02-10 00:09:29 +0100

    TP2 - Exercice 1 finished

--- TP2/Exercice_1/src/TP2_Exercice_1.c
+++ TP2/Exercice_1/src/TP2_Exercice_1.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 int main(void)
 {
-    puts("!!!Hello World!!!");
+    int numberThreads = 6;
+    int threadId = 0;
+
+    omp_set_num_threads(numberThreads);
+
+#pragma omp parallel private(numberThreads, threadId)
+    {
+        threadId = omp_get_thread_num();
+
+        printf("(thread id: %d) Hello, world!\n", threadId);
+
+        if (threadId == 0)
+        {
+            printf("(thread id: %d) There's a total of %d threads working in parallel.", threadId, omp_get_num_threads());
+        }
+    }
+// pragma omp parallel private(numberThreads, threadId)
+
     return EXIT_SUCCESS;
 }
