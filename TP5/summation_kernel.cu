b906641fdcbfc5d2d045a5def225c9c3f0ce3f81
Brandon <yberion.ins@gmail.com>
2020-04-19 18:49:34 +0200

    TP5 - Fix solution 2

--- TP5/summation_kernel.cu
+++ TP5/summation_kernel.cu
@@ -43,7 +43,7 @@ __global__ void summation_kernel_2(int data_size, float* data_out)
 
 	for (int i = 0; i < data_size; ++i)
 	{
-		op = (i % 2 == 0) ? 1 : -1;
+		op = (threadNumber % 2 == 0) ? -1 : 1;
 
 		res += (i == 0 && threadNumber == 0) ? 0 : (float) 1 / (threadNumber + (i * num_threads)) * op;
 	}
