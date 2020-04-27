ada8b5bdbf7507c5a8a8422cfac63b34f261fb2d
Brandon <yberion.ins@gmail.com>
2020-04-28 01:44:51 +0200

    TP6 - Reformat code

--- TP6/life_kernel.cu
+++ TP6/life_kernel.cu
@@ -1,40 +1,47 @@
-
 // Reads a cell at (x+dx, y+dy)
-__device__ int read_cell(int * source_domain, int x, int y, int dx, int dy,
-    unsigned int domain_x, unsigned int domain_y)
+__device__ int read_cell(int * source_domain, int x, int y, int dx, int dy, unsigned int domain_x, unsigned int domain_y)
 {
-    x = (unsigned int)(x + dx) % domain_x;	// Wrap around
+    // Wrap around
+    x = (unsigned int)(x + dx) % domain_x;
     y = (unsigned int)(y + dy) % domain_y;
+
     return source_domain[y * domain_x + x];
 }
 
 // Compute kernel
-__global__ void life_kernel(int * source_domain, int * dest_domain,
-    int domain_x, int domain_y)
+__global__ void life_kernel(int * source_domain, int * dest_domain, int domain_x, int domain_y)
 {
     int tx = blockIdx.x * blockDim.x + threadIdx.x;
     int ty = blockIdx.y * blockDim.y + threadIdx.y;
-    
-    if(tx >= domain_x || ty >= domain_y) return;
+
+    if (tx >= domain_x || ty >= domain_y)
+    {
+        return;
+    }
     
     // Read cell
-    int myself = read_cell(source_domain, tx, ty, 0, 0,
-	                       domain_x, domain_y);
+    int myself = read_cell(source_domain, tx, ty, 0, 0, domain_x, domain_y);
     
     // TODO: Read the 8 neighbors and count number of blue and red
     int redcells = 0;
     int bluecells = 0;
     int cell;
 
-    for(int line=-1; line<2; line++){
-        for(int column=-1; column<2; column++){
-            if(line != 0 && column != 0){   //Do not read myself
-                cell = read_cell(source_domain, tx, ty, line, column,
-                    domain_x, domain_y);
+    for (int line = -1; line < 2; ++line)
+    {
+        for (int column = -1; column < 2; ++column)
+        {
+            //Do not read myself
+            if (line != 0 && column != 0)
+            {
+                cell = read_cell(source_domain, tx, ty, line, column, domain_x, domain_y);
                 
-                if(cell == 1){
+                if (cell == 1)
+                {
                     redcells++;
-                }else if(cell == 2){
+                }
+                else if (cell == 2)
+                {
                     bluecells++;
                 }
             }
@@ -43,11 +50,17 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,
 
 	// TODO: Compute new value
     int sum = redcells + bluecells;
-    int newvalue = 0;   // By default, the cell dies (or stay empty)
+    // By default, the cell dies (or stay empty)
+    int newvalue = 0;
 
-    if(myself == 0 && sum == 3){    // New cell
+    if (myself == 0 && sum == 3)
+    {
+        // New cell
         newvalue = redcells > bluecells ? 1 : 2;
-    }else if(sum == 2 || sum == 3){  // Survives
+    }
+    else if (sum == 2 || sum == 3)
+    {
+        / Survives
         newvalue = myself;
     }
 	
